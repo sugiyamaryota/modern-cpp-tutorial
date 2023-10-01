ec686315dd23e61b91572ededdd4849cdf4ba3f1
sugiyamaryota <guqeeky6va8@gmail.com>
2023-10-01 23:50:36 +0900

    initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+    // This is a comment
+
+    /*
+      This
+      is
+      multi-line
+      comments
+    */
+
+    std::cout << "Hello World!" << '\n';
+    std::cout << "yeahhhhhhhh" << '\n';
+    return 0;
+}
