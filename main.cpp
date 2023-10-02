7aba6f1911d2a991be769132cea1e02f0328fb56
sugiyama_ryota <guqeeky6va8@gmail.com>
2023-10-02 23:31:52 +0900

    variables

--- main.cpp
+++ main.cpp
@@ -2,16 +2,39 @@
 
 int main()
 {
-    // This is a comment
 
-    /*
-      This
-      is
-      multi-line
-      comments
-    */
+    // integer ( whole number )
+    int age = 21;
+    int year = 2023;
+    int days = 7;
 
-    std::cout << "Hello World!" << '\n';
-    std::cout << "yeahhhhhhhh" << '\n';
+    // double (number including decimal)
+    double price = 10.99;
+    double gpa = 2.5;
+    double temperature = 25.1;
+
+    std::cout << price << '\n';
+
+    // single character
+    char grade = 'A';
+    char initial = 'B';
+    char currency = '$';
+
+    std::cout << initial << '\n';
+
+    // boolean(true or false)
+    bool student = false;
+    bool power = true;
+    bool forSale = true;
+
+    // string ( object that represents a sequence of text)
+    std::string name = "Bro";
+    std::string day = "Friday";
+    std::string food = "pizza";
+    std::string address = "123 Fake St.";
+
+    std::cout << "Hello " << name << '\n';
+    std::cout << "You are " << age << " years old" <<'\n';
+
     return 0;
 }
