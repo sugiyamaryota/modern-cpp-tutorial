180f298343bb077fbdf6b16609e6cc59f9158b7f
sugiyama_ryota <guqeeky6va8@gmail.com>
2023-10-03 21:26:01 +0900

    const

--- main.cpp
+++ main.cpp
@@ -2,39 +2,10 @@
 
 int main()
 {
-
-    // integer ( whole number )
-    int age = 21;
-    int year = 2023;
-    int days = 7;
-
-    // double (number including decimal)
-    double price = 10.99;
-    double gpa = 2.5;
-    double temperature = 25.1;
-
-    std::cout << price << '\n';
-
-    // single character
-    char grade = 'A';
-    char initial = 'B';
-    char currency = '$';
-
-    std::cout << initial << '\n';
-
-    // boolean(true or false)
-    bool student = false;
-    bool power = true;
-    bool forSale = true;
-
-    // string ( object that represents a sequence of text)
-    std::string name = "Bro";
-    std::string day = "Friday";
-    std::string food = "pizza";
-    std::string address = "123 Fake St.";
-
-    std::cout << "Hello " << name << '\n';
-    std::cout << "You are " << age << " years old" <<'\n';
+    const double PI = 3.14159;
+    const int LIGHT_SPEED = 299792458;
+    const int WIDTH = 1920;
+    const int HEIGHT = 1080;
 
     return 0;
 }
