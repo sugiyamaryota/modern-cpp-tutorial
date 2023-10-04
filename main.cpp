338368093a9df84efe541f2180c28faea0af936f
sugiyama_ryota <guqeeky6va8@gmail.com>
2023-10-04 22:19:14 +0900

    operation

--- main.cpp
+++ main.cpp
@@ -1,20 +1,12 @@
 #include <iostream>
 #include <vector>
 
-// typedef std::vector<std::pair<std::string, int>> pairlist_t;
-// typedef std::string text_t;
-// typedef int number_t;
-using text_t = std::string;
-using number_t = int;
-
 int main()
 {
-   // pairlist_t pairlist;
-   text_t firstName = "Bro";
-   number_t age = 21;
-
-   std::cout << firstName;
-   std::cout << age;
+    int students = 20;
+    // students *= 3;
+    int remainder = students % 2;
+    std::cout << remainder;
 
     return 0;
 }
