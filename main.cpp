90aee1148d8f3fbf5c5f20394cd083d995a592cc
sugiyama_ryota <guqeeky6va8@gmail.com>
2023-10-05 23:04:23 +0900

    user input

--- main.cpp
+++ main.cpp
@@ -1,20 +1,24 @@
 #include <iostream>
-#include <vector>
+
+// cout << ( insertion operator )
+// cin >> ( extraction operator )
 
 int main()
 {
-    // Implict
-    /**
-        double x = (int)3.14;
-        std::cout << x;
-    */
 
-    // Explict
-    // char x = 100;
-    int correct = 8;
-    int questions = 10;
-    double score = correct/(double)questions*100;
-    std::cout << score << "%";
+    std::string name;
+    int age;
+    std::cout << "what's your name?:";
+    // std::cin >> name;
+
+    std::cout << "what's your age?:";
+    std::cin >> age;
+
+    std::cout << "what's your full name?:";
+    std::getline(std::cin >> std::ws, name); //spaceがある場合をフォロー・getline関数の前にcinを使用すると挙動がおかしくなるので、wsで抽出する
+
+    std::cout << "Hello " << name << '\n';
+    std::cout << "You are " << age << " years old.";
 
     return 0;
 }
