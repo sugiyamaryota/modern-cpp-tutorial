9249db8bcde26bb22aad5cc9d990a2f59b9ee234
sugiyama_ryota <guqeeky6va8@gmail.com>
2023-10-06 23:20:47 +0900

    17 Useful string methods in C++

--- main.cpp
+++ main.cpp
@@ -1,23 +1,38 @@
 #include <iostream>
-#include <cmath>
 
 int main()
 {
-    double temp;
-    char unit;
-    std::cout << "*** Temperature conversion ****\n";
-    std::cout << "F = Fahrenheit\n";
-    std::cout << "C = Celsius\n";
-    std::cout << "What unit would you like to convert to: ";
-    std::cin >> unit;
+    std::string name;
 
-    if(unit == 'F' || unit == 'f'){
-        std::cout << "Enter the temperature in Celsius: ";
-        std::cin >> temp;
+    std::cout << "Enter your name: ";
+    std::getline(std::cin, name);
 
-        temp = (1.8 * temp) +32.0;
-        std::cout << "Temperature is: " << temp << "F\n";
-    }
+    // if(name.length() > 12){
+    //     std::cout << "Your name can't be over 12 characters";
+    // }
+    // else {
+    //     std::cout << "Welcome " << name;
+    // }
+
+    // if(name.empty()){
+    //     std::cout << "Your didn't enter your name";
+    // }
+    // else {
+    //     std::cout << "Hello " << name;
+    // }
+
+    // name.clear();
+    // std::cout << "Hello " << name;
+
+    // name.append("@gmail.com");
+    // std::cout << "Your username is now " << name;
+
+    // std::cout << name.at(0);
+
+    // name.insert(0,"@");
+    // std::cout << name;
+
+    std::cout << name.find(' ');
 
     return 0;
 }
