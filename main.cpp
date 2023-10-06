e4c7e3ed8d9ab41d02aff0ba12962449b1a98e5c
sugiyama_ryota <guqeeky6va8@gmail.com>
2023-10-06 22:01:04 +0900

    11 If statements

--- main.cpp
+++ main.cpp
@@ -1,43 +1,25 @@
 #include <iostream>
 #include <cmath>
 
-/**
- * 以下のリファレンスがある
- * https://cplusplus.com/reference/cmath/
-*/
-
 int main()
 {
+    int age;
 
-    // double x = 3;
-    // double y = 4;
-    // double z;
+    std::cout << "Enter your age: ";
+    std::cin >> age;
 
-    // z = std::max(x,y);
-    // z = std::min(x,y);
-    // z = pow(2,4);
-    // z = sqrt(9);
-
-    // std::cout << z;
-
-    double a;
-    double b;
-    double c;
-
-    std::cout << "Enter side A: ";
-    std::cin >> a;
-
-    std::cout << "Enter side B: ";
-    std::cin >> b;
-
-    /*
-    a = pow(a, 2);
-    b = pow(b, 2);
-    c= sqrt(a+b);
-    */
-    c= sqrt(pow(a, 2)+pow(b, 2));
-
-    std::cout << "side C: " << c;
+    if(age >= 100) {
+        std::cout << "You are too old to enter this site!";
+    }
+    else if(age >= 18){
+        std::cout << "welcome to the site!";
+    }
+    else if(age < 0) {
+        std::cout << "You haven't been born yet!";
+    }
+    else {
+        std::cout << "You are not old enough to enter!";
+    }
 
     return 0;
 }
