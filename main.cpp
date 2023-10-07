6fcdd5c14a0403982f03cf4b3bb4e807589dd0d0
sugiyama_ryota <guqeeky6va8@gmail.com>
2023-10-07 17:38:44 +0900

    22 Nested loops

--- main.cpp
+++ main.cpp
@@ -2,13 +2,23 @@
 
 int main()
 {
-    for(int i = 1; i<=20; i++){
-        if(i ==13){
-            continue;
-        }
-        std::cout << i << '\n';
-    }
+    int rows;
+    int columns;
+    char symbol;
 
+    std::cout << "How many rows?: ";
+    std::cin >> rows;
+    std::cout << "How many columns?: ";
+    std::cin >> columns;
+    std::cout << "Enter a symbol to use:";
+    std::cin >> symbol;
+
+    for(int i = 1; i<=rows; i++){
+        for(int j=1; j <=columns; j++){
+            std::cout << symbol << ' ';
+        }
+        std::cout << '\n';
+    }
 
     return 0;
 }
