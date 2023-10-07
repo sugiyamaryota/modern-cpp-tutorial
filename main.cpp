62ed6b79119cf5847932c07646e5d6aa18ddcedd
sugiyama_ryota <guqeeky6va8@gmail.com>
2023-10-07 17:42:54 +0900

    23 Random number generator

--- main.cpp
+++ main.cpp
@@ -2,23 +2,13 @@
 
 int main()
 {
-    int rows;
-    int columns;
-    char symbol;
-
-    std::cout << "How many rows?: ";
-    std::cin >> rows;
-    std::cout << "How many columns?: ";
-    std::cin >> columns;
-    std::cout << "Enter a symbol to use:";
-    std::cin >> symbol;
-
-    for(int i = 1; i<=rows; i++){
-        for(int j=1; j <=columns; j++){
-            std::cout << symbol << ' ';
-        }
-        std::cout << '\n';
-    }
+    srand(time(NULL));
+    int num1 = (rand() % 6) + 1;
+    int num2 = (rand() % 6) + 1;
+    int num3 = (rand() % 6) + 1;
+    std::cout << num1 << '\n';
+    std::cout << num2 << '\n';
+    std::cout << num3 << '\n';
 
     return 0;
 }
