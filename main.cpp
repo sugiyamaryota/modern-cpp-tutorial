321bd9ab673fe14cbcc3263f657c5d4498725df6
sugiyama_ryota <guqeeky6va8@gmail.com>
2023-10-07 16:54:18 +0900

    18 While loops

--- main.cpp
+++ main.cpp
@@ -4,35 +4,12 @@ int main()
 {
     std::string name;
 
-    std::cout << "Enter your name: ";
-    std::getline(std::cin, name);
+    while(name.empty()){
+        std::cout << "Enter your name: ";
+        std::getline(std::cin, name);
+    }
 
-    // if(name.length() > 12){
-    //     std::cout << "Your name can't be over 12 characters";
-    // }
-    // else {
-    //     std::cout << "Welcome " << name;
-    // }
-
-    // if(name.empty()){
-    //     std::cout << "Your didn't enter your name";
-    // }
-    // else {
-    //     std::cout << "Hello " << name;
-    // }
-
-    // name.clear();
-    // std::cout << "Hello " << name;
-
-    // name.append("@gmail.com");
-    // std::cout << "Your username is now " << name;
-
-    // std::cout << name.at(0);
-
-    // name.insert(0,"@");
-    // std::cout << name;
-
-    std::cout << name.find(' ');
+    std::cout << "Hello" << name;
 
     return 0;
 }
