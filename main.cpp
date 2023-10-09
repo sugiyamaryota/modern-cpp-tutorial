2e3980ac12c7d7255d04bb861d682bbc7cb59699
sugiyama_ryota <guqeeky6va8@gmail.com>
2023-10-09 21:57:10 +0900

    27 Return keyword

--- main.cpp
+++ main.cpp
@@ -1,23 +1,18 @@
 #include <iostream>
 
-double square(double length);
-double cube(double length);
+std::string concatStrings(std::string string1,std::string string2);
 
 int main()
 {
-    double length = 5.0;
-    double area = square(length);
-    double volume = cube(length);
 
-    std::cout << "Area: " << area << "cm^2\n";
-    std::cout << "Volume: " << volume << "cm^3\n";
+    std::string firstName = "Bro";
+    std::string lastName = "Code";
+    std::string fullName = concatStrings(firstName, lastName);
+    std::cout << "Hello " << fullName << "\n";
 
     return 0;
 }
 
-double square(double length){
-    return length * length;
-}
-double cube(double length){
-    return length * length * length;
+std::string concatStrings(std::string string1,std::string string2){
+    return string1 + " " + string2;
 }
