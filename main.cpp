fcf18d835763f819fd05bafca393e6e6f0148c5c
sugiyama_ryota <guqeeky6va8@gmail.com>
2023-10-09 23:43:09 +0900

    30 Banking practice program

--- main.cpp
+++ main.cpp
@@ -1,19 +1,73 @@
 #include <iostream>
+#include <iomanip>
 
-int myNum = 3;
-
-void printNum();
+void showBalance(double balance);
+double deposit();
+double withdraw(double balance);
 
 int main()
 {
-    int myNum = 1;
-    printNum();
-    std::cout << ::myNum << "\n";
+    double balance = 123.01;
+    int choice = 0;
+    do{
+        std::cout << "****************\n";
+        std::cout << "Enter your choice:\n";
+        std::cout << "****************\n";
+        std::cout << "1. Show Balance\n";
+        std::cout << "2. Deposit Money\n";
+        std::cout << "3. Withdraw Money\n";
+        std::cout << "4. Exit\n";
+        std::cin >> choice;
+
+        std::cin.clear();
+        fflush(stdin);
+
+        switch(choice){
+            case 1: showBalance(balance);
+                break;
+            case 2: balance += deposit();
+                break;
+            case 3: balance -= withdraw(balance);
+                break;
+            case 4: std::cout << "Thanks for visiting!\n";
+                break;
+            default: std::cout << "Invalid choice\n";
+        }
+    }while(choice != 4);
 
     return 0;
 }
+void showBalance(double balance){
+    std::cout << "Your balance is: $" << std::setprecision(2) << std::fixed << balance << "\n";
+}
+double deposit(){
+    double amount = 0;
+    std::cout << "Enter amount to be deposited: \n";
+    std::cin >> amount;
 
-void printNum(){
-    int myNum = 2;
-    std::cout << ::myNum << "\n";
+    if(amount > 0){
+        return amount;
+    }
+    else{
+        std::cout << "Enter amount to be deposited: \n";
+        return 0;
+    }
+}
+double withdraw(double balance){
+    double amount = 0;
+    std::cout << "Enter amount to be withdrawn: \n";
+    std::cin >> amount;
+
+    if(amount > balance){
+        std::cout << "insufficient funds\n";
+        return 0;
+    }
+    else if(amount < 0){
+        std::cout << "That's not a valid amount\n";
+    }
+    else {
+        return amount;
+    }
+
+    return amount;
 }
