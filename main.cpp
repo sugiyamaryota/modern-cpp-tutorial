740310d6cd5dd1f3dff05f9567230062fbbdc7cb
sugiyama_ryota <guqeeky6va8@gmail.com>
2023-10-12 22:48:45 +0900

    33 Sizeof() operator

--- main.cpp
+++ main.cpp
@@ -2,28 +2,14 @@
 
 int main()
 {
+    std::string name = "Bro Code ";
+    double gpa = 2.5;
+    char grade = 'F';
+    bool student = true;
+    char grades[] = {'A','B','C','D','F'};
+    std::string students[] = {"Spongebob","Patrick","Squidward"};
 
-    // std::string car[] = {"Corvette","Mustang","Carmy"};
-    std::string car[3];
-    car[0] = "Camaro";
-    car[1] = "Mustang";
-    car[2] = "Carmy";
-
-    std::cout << car[0] << '\n';
-    std::cout << car[1] << '\n';
-    std::cout << car[2] << '\n';
-
-    // double prices[] = {5.00,7.50,9.99,15.00};
-    double prices[4];
-    prices[0] = 5.00;
-    prices[1] = 7.50;
-    prices[2] = 9.99;
-    prices[3] = 15.00;
-
-    std::cout << prices[0] << '\n';
-    std::cout << prices[1] << '\n';
-    std::cout << prices[2] << '\n';
-    std::cout << prices[3] << '\n';
+    std::cout << sizeof(students)/sizeof(std::string) << " elements\n";
 
     return 0;
 }
