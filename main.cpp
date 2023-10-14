e0433804a9c9647dd3befc0e744711c0a74be553
sugiyama_ryota <guqeeky6va8@gmail.com>
2023-10-14 23:33:55 +0900

    39 Fill() function

--- main.cpp
+++ main.cpp
@@ -1,29 +1,17 @@
 #include <iostream>
 
-int sort(int array[], int size);
-
 int main()
 {
-    int array[] = {10,1,9,2,8,3,7,4,6,5};
-    int size = sizeof(array)/sizeof(array[0]);
+    const int SIZE = 99;
+    std::string foods[SIZE];
 
-    sort(array, size);
+    fill(foods, foods + SIZE, "pizza");
+    fill(foods + (SIZE/3), foods + (SIZE/3)*2, "hambuger");
+    fill(foods + (SIZE/3)*2, foods + SIZE, "hotdog");
 
-    for(int element: array){
-        std::cout << element << '\n';
+    for(std::string food : foods){
+        std::cout << food << '\n';
     }
 
     return 0;
-}
-int sort(int array[], int size){
-    int temp;
-    for(int i = 0; i < size - 1; i++){
-        for(int j = 0; j < size - 1; j++){
-            if(array[j] > array[j+1]){
-                temp = array[j];
-                array[j] = array[j+1];
-                array[j + 1] = temp;
-            }
-        }
-    }
 }
