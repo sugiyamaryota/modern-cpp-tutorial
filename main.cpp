d3acdc9997ce44274236fd23933b3bd718fb9675
sugiyama_ryota <guqeeky6va8@gmail.com>
2023-10-17 23:03:49 +0900

    42 QUIZ GAME

--- main.cpp
+++ main.cpp
@@ -2,21 +2,49 @@
 
 int main()
 {
-    std::string cars[][3] = {
-        {"Mustang","Escap","F-150"},
-        {"Corvette","Equinox","Silverado"},
-        {"Challenger","Durango","Ram 1500"}
+
+    std::string questions[] = { "1. What year was C++ created?:",
+                                "2. Who invented C++?:",
+                                "3. What is the producessor of C++?:",
+                                "4. is the earth flat?:",
     };
+    std::string options[][4] = {
+        {"A. 1689","B. 1975","C. 1985","D. 1989"},
+        {"A. Guide van Rossum","B. Bjare Stroustup","C. John Carmack","D. Mark Zuckerburg"},
+        {"A. C","B. C+","C. C--","D. B++"},
+        {"A. yes","B. no","C. sometimes","D. what`s Earth?"},
+    };
+    char answerKey[] = {'C','B','A','B'};
 
-    int rows = sizeof(cars)/sizeof(cars[0]);
-    int columns = sizeof(cars)/sizeof(cars[0][0]);
+    int size = sizeof(questions)/sizeof(questions[0]);
+    char guess;
+    int score;
 
-    for(int i = 0; i < rows; i++){
-        for(int j = 0; j < columns; j++){
-            std::cout << cars[i][j] << " ";
+
+    for(int i = 0; i < size; i++){
+        std::cout << "******************************************" << '\n';
+        std::cout << questions[i] << '\n';
+        std::cout << "******************************************" << '\n';
+        for(int j = 0; j < sizeof(options[i])/sizeof(options[i][0]); j++){
+            std::cout << options[i][j] << '\n';
         }
-        std::cout << '\n';
+        std::cin >> guess;
+        guess = toupper(guess);
+        if(guess == answerKey[i]){
+            std::cout << "CORRECT" << '\n';
+            score++;
+        } else {
+            std::cout << "WRONG" << '\n';
+            std::cout << "Answer:" << answerKey[i] << '\n';
+        }
     }
+
+    std::cout << "******************************************" << '\n';
+    std::cout << "*          RESULTS          *" << '\n';
+    std::cout << "******************************************" << '\n';
+    std::cout << "CORRECT GUESS: " << score << '\n';
+    std::cout << "# of QUESTIONS: " << size << '\n';
+    std::cout << "SCORE: " << (score/(double)size)*100 << "%" << '\n';
 
     return 0;
 }
