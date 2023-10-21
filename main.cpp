b16611e1dd4a42d04cf627f58f21ac0a5dbdc2a7
sugiyama_ryota <guqeeky6va8@gmail.com>
2023-10-21 12:52:11 +0900

    46 Credit card validator program

--- main.cpp
+++ main.cpp
@@ -1,20 +1,40 @@
 #include <iostream>
 
-void printInfo(const std::string &name,const int &age);
+int getDigit(const int number);
+int sumOddDigit(const std::string cardNumber);
+int sumEvenDigit(const std::string cardNumber);
 
 int main()
 {
 
-    std::string name = "Bro";
-    int age = 21;
+    std::string cardNumber;
+    int result = 0;
 
-    printInfo(name,age);
+    std::cout << "Enter a credit card #: " << '\n';
+    std::cin >> cardNumber;
 
+    result = sumEvenDigit(cardNumber) + sumOddDigit(cardNumber);
+    if(result % 10 == 0){
+        std::cout << cardNumber << " is valid" << '\n';
+    }else {
+        std::cout << cardNumber << " is not valid" << '\n';
+    }
     return 0;
 }
-void printInfo(const std::string &name, const int &age){
-    // name = " ";
-    // age = 0;
-    std::cout << name << '\n';
-    std::cout << age << '\n';
+int getDigit(const int number){
+    return number % 10 + (number / 10 % 10);
+}
+int sumOddDigit(const std::string cardNumber){
+    int sum = 0;
+    for(int i = cardNumber.size() - 1; i >=0; i-=2 ){
+        sum += cardNumber[i] - '0';
+    }
+    return sum;
+}
+int sumEvenDigit(const std::string cardNumber){
+    int sum = 0;
+    for(int i = cardNumber.size() - 2; i >=0; i-=2 ){
+        sum += getDigit((cardNumber[i] - '0')*2);
+    }
+    return sum;
 }
