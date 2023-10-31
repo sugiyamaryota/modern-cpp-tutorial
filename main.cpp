8d9582f152eb3dd10ccc2c0bb4c5bd74e9b49442
sugiyama_ryota <guqeeky6va8@gmail.com>
2023-10-31 21:59:22 +0900

    56 Object Oriented Programming

--- main.cpp
+++ main.cpp
@@ -1,30 +1,63 @@
 #include <iostream>
 
-enum Day {sunday = 0, monday = 1, tuesday = 2, wednesday = 3,
-            thursday = 4, friday = 5, saturday = 6};
+class Human{
+    public:
+        std::string name = "Rick";
+        std::string occupation = "scientist";
+        int age;
+        void eat(){
+            std::cout << "This person is eating" <<'\n';
+        }
+        void drink(){
+            std::cout << "This person is drinking" <<'\n';
+        }
+        void sleep(){
+            std::cout << "This person is sleeping" <<'\n';
+        }
+};
+class Car{
+    public:
+        std::string make;
+        std::string model;
+        int year;
+        std::string color;
+        void accelerate(){
+            std::cout << "You step on the gas!" <<'\n';
+        }
+        void brake(){
+            std::cout << "You step on the breaks!" <<'\n';
+        }
+};
 int main()
 {
 
-    Day today = friday;
-    switch (today)
-    {
-    case sunday: std::cout <<  "It is Sunday!" <<'\n';
-        break;
-    case monday: std::cout <<  "It is Monday!" <<'\n';
-        break;
-    case tuesday: std::cout <<  "It is Tuesday!" <<'\n';
-        break;
-    case wednesday: std::cout <<  "It is Wednesday!" <<'\n';
-        break;
-    case thursday: std::cout <<  "It is Thursday!" <<'\n';
-        break;
-    case friday: std::cout <<  "It is Friday!" <<'\n';
-        break;
-    case saturday: std::cout <<  "It is Saturday!" <<'\n';
-        break;
-    default:
-        break;
-    }
+    // Human human1;
+
+    // human1.name = "Rick";
+    // human1.occupation = "scientist";
+    // human1.age = 70;
+
+    // std::cout << human1.name <<'\n';
+    // std::cout << human1.occupation <<'\n';
+    // std::cout << human1.age <<'\n';
+
+    // human1.eat();
+    // human1.drink();
+    // human1.sleep();
+
+    Car car1;
+    car1.make = "Ford";
+    car1.model = "Mustang";
+    car1.year = 2023;
+    car1.color = "silver";
+
+    std::cout << car1.make <<'\n';
+    std::cout << car1.model <<'\n';
+    std::cout << car1.year <<'\n';
+    std::cout << car1.color <<'\n';
+
+    car1.accelerate();
+    car1.brake();
 
     return 0;
 }
