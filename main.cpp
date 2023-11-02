ea6029901a789f108b1d869f88ec9a23b921518a
sugiyama_ryota <guqeeky6va8@gmail.com>
2023-11-02 21:49:40 +0900

    58 Constructor overloading

--- main.cpp
+++ main.cpp
@@ -1,42 +1,28 @@
 #include <iostream>
 
-class Student{
+class Pizza{
     public:
-        std::string name;
-        int age;
-        double gpa;
-    Student(std::string name, int age, double gpa){
-        this->name = name;
-        this->age = age;
-        this->gpa = gpa;
+        std::string topping1;
+        std::string topping2;
+    Pizza(){
+
     }
-};
-class Car{
-    public:
-        std::string make;
-        std::string model;
-        int year;
-        std::string color;
-    Car(std::string make, std::string model, int year, std::string color){
-        this->make = make;
-        this->model = model;
-        this->year = year;
-        this->color = color;
+    Pizza(std::string topping1){
+        this->topping1 = topping1;
+    }
+    Pizza(std::string topping1,std::string topping2){
+        this->topping1 = topping1;
+        this->topping2 = topping2;
     }
 };
 int main()
 {
+    Pizza pizza1("pepperoni");
+    Pizza pizza2("mushrooms","peppers");
+    Pizza pizza3;
 
-    Student student1("Spongebob", 25, 3.2);
-    std::cout << student1.name <<'\n';
-    std::cout << student1.age <<'\n';
-    std::cout << student1.gpa <<'\n';
-
-    Car car1("Chevy", "Corvette", 2022, "blue");
-    std::cout << car1.make <<'\n';
-    std::cout << car1.model <<'\n';
-    std::cout << car1.year <<'\n';
-    std::cout << car1.color <<'\n';
+    std::cout << pizza1.topping1 <<'\n';
+    std::cout << pizza2.topping1 <<'\n';
 
     return 0;
 }
