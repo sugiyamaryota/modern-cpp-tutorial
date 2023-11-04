42e677dfe7bf6db71d5eff1212040d6b7a34e0ad
sugiyama_ryota <guqeeky6va8@gmail.com>
2023-11-04 12:44:53 +0900

    60 Inheritance

--- main.cpp
+++ main.cpp
@@ -1,34 +1,64 @@
 #include <iostream>
 
-class Stove{
+// class Animal{
+//     public:
+//         bool alive = true;
+//     void eat(){
+//         std::cout << "This animal is eating\n";
+//     }
+// };
+// class Dog : public Animal {
+//     public:
+//     void bark(){
+//         std::cout << "The dog goes woof!\n";
+//     }
+// };
+// class Cat : public Animal {
+//     public:
+//     void meow(){
+//         std::cout << "The cat goes meow!\n";
+//     }
+// };
+class Shape{
     public:
-        int temperature = 0;
+        double area;
+        double volume;
+};
+class Cube: public Shape{
     public:
-    Stove(int temperature){
-        setTemperature(temperature);
-    };
-    int getTemperature(){
-        return temperature;
+        double side;
+    Cube(double side){
+        this->side = side;
+        this->area = side * side * 6;
+        this->volume = side * side * side;
     }
-    void setTemperature(int temperature){
-        if(temperature < 0){
-            this->temperature = 0;
-        }
-        else if(temperature >= 10){
-            this->temperature = 10;
-        }
-        else{
-            this->temperature = temperature;
-        }
-    };
+};
+class Sphere: public Shape{
+    public:
+        double radius;
+    Sphere(double radius){
+        this->radius = radius;
+        this->area = 4 * 3.14159 * (radius * radius);
+        this->volume = (4/3.0) * 3.14159 * (radius * radius * radius);
+    }
 };
 int main()
 {
+    // Dog dog;
+    // Cat cat;
 
-    Stove stove(0);
-    stove.setTemperature(100000);
+    // std::cout  << cat.alive <<'\n';
+    // cat.eat();
+    // cat.meow();
 
-    std::cout << "The temperature setting is: " << stove.getTemperature() <<'\n';
+    Cube cube(10);
+    Sphere sphere(5);
+
+    std::cout << "Area: " << cube.area << "cm" <<'\n';
+    std::cout << "Volume: " << cube.volume << "cm" <<'\n';
+
+    std::cout << "Area: " << sphere.area << "cm" <<'\n';
+    std::cout << "Volume: " << sphere.volume << "cm" <<'\n';
 
     return 0;
 }
