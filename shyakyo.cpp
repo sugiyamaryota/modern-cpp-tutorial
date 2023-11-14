97addb657ba2489eabf3d4b454d89b36859ea030
sugiyama_ryota <guqeeky6va8@gmail.com>
2023-11-14 21:31:46 +0900

    11

--- shyakyo.cpp
+++ shyakyo.cpp
@@ -1,31 +1,29 @@
-#include <bitset>
 #include <iostream>
 #include <string>
+#include <utility>
+#include <vector>
 
-auto gray_encode(unsigned int const num) -> unsigned int {
-    return num ^ (num >> 1);
-}
-
-auto gray_decode(unsigned int gray) -> unsigned int {
-    for(unsigned int bit = 1U << 31; bit > 1; bit >>= 1){
-        if((gray & bit) != 0U){
-            gray ^= bit >> 1;
+auto to_roman(unsigned int value) -> std::string {
+    std::vector<std::pair<unsigned int, char const*>> const roman{
+        {1000,"M"},{900,"CM"},{500,"D"},{400,"CD"},{100,"C"},
+        {90,"XC"}, {50,"L"}, {40,"XL"}, {10,"X"}, {9,"IX"},
+        {5, "V"}, {4, "IV"}, {1, "I"}};
+    std::string result;
+    for(auto const& [num, str] : roman){
+        while(value >= num){
+            result += str;
+            value -= num;
         }
     }
-    return gray;
-}
-
-auto to_binary(unsigned int const value, int const digits) -> std::string {
-    return std::bitset<32>(value).to_string().substr(32 - digits, digits);
+    return result;
 }
 
 auto main(int /*argc*/, const char* /*argv*/[]) -> int{
-    std::cout << "Number\tBinary\tGray\tDecoded" <<std::endl;
-    std::cout << "-------\t-----\t----\t--------" <<std::endl;
-    for(unsigned int n = 0; n < 32; ++n){
-        auto encg = gray_encode(n);
-        auto decg = gray_decode(encg);
-        std::cout << n << "\t" << to_binary(n,5) << "\t" << to_binary(encg,5)
-            << "\t" << decg << std::endl;
+    for(int i = 0; i < 100; ++i){
+        std::cout << i << "\t" << to_roman(i) << std::endl;
     }
+    int number = 0;
+    std::cout << "number:";
+    std::cin >> number;
+    std::cout << to_roman(number) << std::endl;
 }
